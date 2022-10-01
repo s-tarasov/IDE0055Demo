3421b14ec9fd28c84e30cb5ec0ae8065572eea68
Stanislav Tarasov <mr.netpilgrim@gmail.com>
2022-10-02 00:06:40 +0300

    test

--- IDE0055Demo/Class1.cs
+++ IDE0055Demo/Class1.cs
@@ -3,7 +3,7 @@ public class Class1
 {
     static void Method1()
     {
-        Enumerable.Range(1, 1)
+        Enumerable.Range(1,  1)
             .Select(x => x.ToString())
             // Comment here cause warnings when LF.
             .ToArray();
