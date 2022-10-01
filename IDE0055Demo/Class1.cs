c1427b4c85962609667bce73027b8e87df3f331b
Stanislav Tarasov <mr.netpilgrim@gmail.com>
2022-10-01 23:56:12 +0300

    init

---
+++ IDE0055Demo/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace IDE0055Demo;
+public class Class1
+{
+    static void Method1()
+    {
+        Enumerable.Range(1, 1)
+            .Select(x => x.ToString())
+            // Comment here cause warnings when LF.
+            .ToArray();
+
+    }
+}
